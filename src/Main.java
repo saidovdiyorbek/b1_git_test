2572e9a29cc56f57250f85dfab1147b23a795596
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-07 09:22:32 +0500

    main ni ozgartirdim zor qibmanmi

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Bu endi oddiy main method emas men ozgarish kiritdim meni kimligimni bilasanmi");
+        System.out.println("hohlaganimcha ozgarish qilaman nima qila olasan");
+
     }
 }
